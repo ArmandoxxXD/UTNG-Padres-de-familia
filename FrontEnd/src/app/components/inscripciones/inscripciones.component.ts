2cd0e4eb34ca9792bee27dad237f94498a20b7b2
MMarioLP <am4063228@gmail.com>
2023-11-28 04:06:14 -0600

    patita de pollo

--- FrontEnd/src/app/components/inscripciones/inscripciones.component.ts
+++ FrontEnd/src/app/components/inscripciones/inscripciones.component.ts
@@ -1,23 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-inscripciones',
   templateUrl: './inscripciones.component.html',
   styleUrls: ['./inscripciones.component.css']
 })
-export class InscripcionesComponent implements OnInit {
+export class InscripcionesComponent implements OnInit, AfterViewInit {
   requisitos = [
     '1.- CURP',
     '2.- Comprobante de pago',
     '3.- Carta laboral en caso de elegir la modalidad semiescolarizada'
   ];
-  constructor() { }
+
+  mostrarCostos: boolean = false;
+  mostrarRequisitos: boolean = false;
+  
+  isCollapseCostosExpanded: boolean = false;
+  isCollapseRequisitosExpanded: boolean = false;
 
   ngOnInit(): void {
   }
 
-  mostrarCostos: boolean = false;
-  mostrarRequisitos: boolean = false;
+  ngAfterViewInit() {
+    const collapseCostos = document.getElementById('collapseCostos');
+    const collapseRequisitos = document.getElementById('collapseRequisitos');
+
+    if (collapseCostos) {
+      collapseCostos.addEventListener('shown.bs.collapse', () => this.scrollIntoView(collapseCostos));
+      collapseCostos.addEventListener('hidden.bs.collapse', () => this.scrollIntoView(collapseCostos));
+    }
+
+    if (collapseRequisitos) {
+      collapseRequisitos.addEventListener('shown.bs.collapse', () => this.scrollIntoView(collapseRequisitos));
+      collapseRequisitos.addEventListener('hidden.bs.collapse', () => this.scrollIntoView(collapseRequisitos));
+    }
+  }
 
   toggleCostos(): void {
     this.mostrarCostos = !this.mostrarCostos;
@@ -27,4 +44,8 @@ export class InscripcionesComponent implements OnInit {
     this.mostrarRequisitos = !this.mostrarRequisitos;
   }
 
+  // Función para hacer scroll a la sección especificada.
+  scrollIntoView(element: HTMLElement): void {
+    element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  }
 }
