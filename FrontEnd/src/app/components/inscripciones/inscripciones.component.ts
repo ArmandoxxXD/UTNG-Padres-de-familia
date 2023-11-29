3dda59eec4296c51e5847ff6019b97ca7d2bcd23
Osmar Villegas <osmarvm25@gmail.com>
2023-11-29 07:01:52 -0600

    Text to Speech en inscripciones

--- FrontEnd/src/app/components/inscripciones/inscripciones.component.ts
+++ FrontEnd/src/app/components/inscripciones/inscripciones.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { ModoOscuroService } from 'src/app/services/modo-oscuro.service';
 
+import {LecturaService} from 'src/app/services/lectura.service' 
 
 @Component({
   selector: 'app-inscripciones',
@@ -24,7 +25,7 @@ export class InscripcionesComponent implements OnInit, AfterViewInit {
   isCollapseRequisitosExpanded: boolean = false;
   esModoOscuro: boolean = false;
 
-  constructor(private modoOscuroService:ModoOscuroService) { }
+  constructor(private modoOscuroService:ModoOscuroService, public lectura: LecturaService) { }
 
 
   ngOnInit(): void {
